543a717be50e4c319e68b8c7f876302e89a2d67d
jawadDouf <jdoufare2001@gmail.com>
2023-04-06 04:46:30 +0000

    setup the router for the visible pages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LandingPageComponent } from './homepage/landing-page/landing-page.component';
+import { AuthentificationComponent } from './authFeature/pages/authentification/authentification.component';
+import { DashboardMainSectionComponent } from './dashboard-main-section/dashboard-main-section.component';
 
 const routes: Routes = [
   // { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
   { path: 'home', component: LandingPageComponent,},
-  // {
-  //   path: 'dashboard',
-  //   component: PagesComponent,
+  { path: 'authentification',component: AuthentificationComponent},
+  {
+     path: 'dashboard',
+     component: DashboardMainSectionComponent,
   //   children: [
   //     { path: '', pathMatch: 'full', redirectTo: 'upload-document' },
   //     {
@@ -21,7 +24,7 @@ const routes: Routes = [
   //     { path: 'upload-document', component: UploadDocComponent },
   //     { path: 'collaborator', component: CollaborateursComponent },
   //   ],canActivate:[AuthGuard]
-  // }
+   }
 ];
 
 @NgModule({
